44ccd851347166fb8c4a554d4b143a6dff98500a
azri <azri@azri-System-Product-Name.(none)>
2013-10-25 19:45:13 +0530

    scaffold Gallery and migration is done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoblog::Application.config.secret_token = '76b68184f2b49fd2a728309bae089a4135226ee8c221b167e709c3f2da602dba2bd0d6be86e181a85f3aab41cb43cf2bdeab9156fe586436e462ab2fab979ff9'
