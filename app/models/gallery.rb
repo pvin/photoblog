367e0b7880db378906da8383981c5c76ca56f805
azri <azri@azri-System-Product-Name.(none)>
2013-10-28 11:54:07 +0530

    #established one many ralationship

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,5 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :title
   has_many :photos
+  #established one many ralationship
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
   attr_accessible :gallery_id, :title
   belongs_to :gallery
+  #established one many ralationship
 end
